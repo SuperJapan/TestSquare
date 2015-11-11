ccd9bbf5c52b90148eca58345a95315e7cdb56f7
TestCat <serverov2014@yandex.ru>
2015-11-11 22:12:14 +0400

    Update UtilSquare

---
+++ UnitTestUtilSquare/UnitTestUtilSq.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UtilSquare;
+
+namespace UnitTestUtilSquare
+{
+    [TestClass]
+    public class UnitTestUtilSq
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double expected = 1298;
+            
+            double a = 44; double b = 59;
+            double square = UtilMath.getSquareTriangle(a, b);
+            Console.WriteLine("square:" + square);
+
+            double actual = square;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+    }
+}
---
+++ UtilSquare/UtilMath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilSquare
+{
+    public class UtilMath
+    {
+        public static double getSquareTriangle(double a, double b)
+        {
+            return (a * b) / 2;
+        }
+        
+    }
+}
